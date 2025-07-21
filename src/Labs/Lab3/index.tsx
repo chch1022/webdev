2eba76e52ea657f144fc2f16a4a2f6da97ae0358
Chen Chen <chenchen102259@gmail.com>
2025-07-21 13:50:56 -0700

    Finish 3.6

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,6 +1,6 @@
-import BooleanVariables from './BooleanVariables';  
+import BooleanVariables from './BooleanVariables';
 import ConditionalOutputInline from './ConditionalOutputInline';
-import ConditionalOutputIfElse from './ConditionalOutputIfElse'; 
+import ConditionalOutputIfElse from './ConditionalOutputIfElse';
 import TernaryOperator from './TernaryOperator';
 import IfElse from './IfElse';
 import VariableTypes from './VariableTypes';
@@ -8,6 +8,28 @@ import VariablesAndConstants from './VariablesAndConstants';
 import LegacyFunctions from './LegacyFunctions';
 import ArrowFunctions from './ArrowFunctions';
 import ImpliedReturn from './ImpliedReturn';
+import SimpleArrays from './SimpleArrays';
+import ArrayIndexAndLength from './ArrayIndexAndLength';
+import AddingAndRemovingToFromArrays from './AddingAndRemovingToFromArrays';
+import ForLoops from './ForLoops';
+import MapFunction from './MapFunction';
+import FindFunction from './FindFunction';
+import FindIndex from './FindIndex';
+import FilterFunction from './FilterFunction';
+import JsonStringify from './JsonStringify';
+import House from './House';
+import TodoList from './todos/TodoList';
+import Spreading from './Spreading';
+import Destructing from './Destructing';
+import FunctionDestructing from './FunctionDestructing';
+import DestructingImports from './DestructingImports';
+import Classes from './Classes';
+import Styles from './Styles';
+import Add from './Add';
+import Square from './Square';
+import Highlight from './Highlight';
+import AddPathParameters from './AddPathParameters';
+import PathParameters from './PathParameters';
 
 export default function Lab3() {
   return (
@@ -18,10 +40,44 @@ export default function Lab3() {
       <BooleanVariables />
       <IfElse />
       <TernaryOperator />
-      <ConditionalOutputIfElse /> 
+      <ConditionalOutputIfElse />
       <ConditionalOutputInline />
       <LegacyFunctions />
       <ArrowFunctions />
       <ImpliedReturn />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <div id="wd-lab3">
+        <h3>JavaScript</h3>
+        <h4>Square of 4</h4>
+        <Square>4</Square>
+        <hr />
+      </div>
+       <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+     <AddPathParameters />
+     <PathParameters />
+
+
     </div>
-);}
+  );
+}
+
