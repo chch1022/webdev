be3b70fffd77c868a475f9d46e3ee2db6a736882
Chen Chen <chenchen102259@gmail.com>
2025-08-05 16:44:23 -0700

    Fix index.js again

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -1,29 +1,31 @@
-import "dotenv/config";  // Add this line at the very top
+import "dotenv/config";
 import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
+import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import session from "express-session";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
-import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
-import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
-
 
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    "https://chenchen-summer-2025.netlify.app/",
+    origin: [
+      process.env.CLIENT_URL || "http://localhost:5173",
+      "https://chenchen-summer-2025.netlify.app"
+    ],
   })
 );
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.SERVER_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -32,16 +34,16 @@ if (process.env.SERVER_ENV !== "development") {
     domain: process.env.SERVER_URL,
   };
 }
+
 app.use(session(sessionOptions));
 app.use(express.json());
 
 UserRoutes(app);
 CourseRoutes(app);
-EnrollmentRoutes(app);
+AssignmentRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000);
-AssignmentRoutes(app);
 
+app.listen(process.env.PORT || 4000);
 console.log("Server running on http://localhost:4000");
