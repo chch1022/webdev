afc9bde94721c84ccebadb54be84606683654b03
Chen Chen <chenchen102259@gmail.com>
2025-08-07 18:00:56 -0700

    fix sign in

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -10,8 +10,8 @@ export default function Signin() {
   const [credentials, setCredentials] = useState<any>({});
   const dispatch = useDispatch();
   const navigate = useNavigate();
-   const signin = async () => {
-    const user =  await client.signin(credentials);
+  const signin = async () => {
+    const user = await client.signin(credentials); // Make sure client.signin uses axiosWithCredentials
     if (!user) return;
     dispatch(setCurrentUser(user));
     navigate("/Kambaz/Dashboard");
@@ -21,12 +21,13 @@ export default function Signin() {
     <div id="wd-signin-screen">
       <h1>Sign in</h1>
       <FormControl defaultValue={credentials.username}
-             onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-             className="mb-2" placeholder="username" id="wd-username" />
+        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+        className="mb-2" placeholder="username" id="wd-username" />
       <FormControl defaultValue={credentials.password}
-             onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-             className="mb-2" placeholder="password" type="password" id="wd-password" />
+        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+        className="mb-2" placeholder="password" type="password" id="wd-password" />
       <Button onClick={signin} id="wd-signin-btn" className="w-100" > Sign in </Button>
       <Link id="wd-signup-link" to="/Kambaz/Account/Signup"> Sign up </Link>
     </div>
-);}
+  );
+}
