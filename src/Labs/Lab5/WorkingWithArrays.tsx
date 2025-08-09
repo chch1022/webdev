ea644baaadb9846e615c6e9f875db2bf84b1f0e3
Chen Chen <chenchen102259@gmail.com>
2025-08-09 00:57:49 -0700

    remove import

--- src/Labs/Lab5/WorkingWithArrays.tsx
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -10,7 +10,7 @@ export default function WorkingWithArrays() {
         due: "2021-09-09",
         completed: false,
     });
-    const [editingTodo, setEditingTodo] = useState({ id: 1, description: '', completed: false });
+    //const [editingTodo, setEditingTodo] = useState({ id: 1, description: '', completed: false });
     return (
         <div id="wd-working-with-arrays">
             <h3>Working with Arrays</h3>
