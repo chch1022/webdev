13f9231702b86998397239599e8f598cab236ec4
Chen Chen <chen.chencc@outlook.com>
2025-08-17 01:23:08 -0700

    see if this fix crashing

--- src/Kambaz/Courses/reducer.ts
+++ src/Kambaz/Courses/reducer.ts
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 //import { courses } from "../Database";
 import { v4 as uuidv4 } from "uuid";
+import { courses } from "../Database";
 
 const initialState = {
-    courses: [],
+    courses: courses,
 };
 
 const coursesSlice = createSlice({
